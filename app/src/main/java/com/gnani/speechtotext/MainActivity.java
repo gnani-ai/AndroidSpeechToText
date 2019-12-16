63e1c94b026d7e58db496246dc31cc0ac132fa6b
gnani-ai <swapnil@gnani.ai>
2019-12-16 11:54:38 +0530

    Demo app code

--- app/src/main/java/com/gnani/speechtotext/MainActivity.java
+++ app/src/main/java/com/gnani/speechtotext/MainActivity.java
@@ -7,12 +7,8 @@ import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.TextView;
-import android.widget.Toast;
 
-import com.gnani.stt.Recorder;
-import com.gnani.stt.SpeechService;
-
-public class MainActivity extends AppCompatActivity implements SpeechService.Listener, Recorder.RecordingStatusListener {
+public class MainActivity extends AppCompatActivity {
 
     private Button btnS;
     private TextView txtT;
@@ -22,8 +18,6 @@ public class MainActivity extends AppCompatActivity implements SpeechService.Lis
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        Recorder.bind(MainActivity.this, MainActivity.this, MainActivity.this);
-
         btnS = findViewById(R.id.btnS);
         txtT = findViewById(R.id.txtT);
 
@@ -31,44 +25,8 @@ public class MainActivity extends AppCompatActivity implements SpeechService.Lis
             @Override
             public void onClick(View view) {
 
-                Recorder.onRecord(BuildConfig.token, BuildConfig.language, BuildConfig.accesskey, BuildConfig.audioformat, BuildConfig.encoding, BuildConfig.sad, BuildConfig.ip, BuildConfig.port, BuildConfig.tls);
 
             }
         });
-    }
-
-    @Override
-    public void onRecordingStatus(boolean status) {
-
-        Log.e("STATUS", " " + status);
-
-        if (status) {
-            btnS.setText("STOP");
-        } else {
-            btnS.setText("START");
-        }
-
-    }
-
-    @Override
-    public void onSpeechRecognized(final String text, String asr, boolean isFinal) {
-
-        Log.e("text", " " + text);
-        runOnUiThread(new Runnable() {
-            @Override
-            public void run() {
-
-                txtT.setText(text);
-
-            }
-        });
-
-    }
-
-    @Override
-    protected void onDestroy() {
-        super.onDestroy();
-
-        Recorder.unbind(MainActivity.this);
     }
 }
